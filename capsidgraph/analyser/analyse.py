8a0f8c8a95ffd316d8cad571b91e64a310cd6281
Quentin Roussel <contact@quentinrsl.com>
2023-07-13 22:20:32 +0100

    code formating

--- capsidgraph/analyser/analyse.py
+++ capsidgraph/analyser/analyse.py
@@ -5,50 +5,90 @@ from inspect import signature
 
 
 def bisection_stop_condition(n: int, pfrag: float, settings: Dict) -> bool:
-	INV_PROBA = 1 / settings.get("error_probability", 0.05)
-	max_iterations = settings.get("max_iterations", 1000000)
-	min_iterations = settings.get("min_iterations", 1000)
-	return n >= min_iterations and (
-		4 * n * ((pfrag - 0.5) ** 2) >= INV_PROBA or n >= max_iterations
-	)
+    INV_PROBA = 1 / settings["error_probability"]
+    max_iterations = settings.get("max_iterations", 1000000)
+    min_iterations = settings.get("min_iterations", 1000)
+    return n >= min_iterations and (
+        4 * n * ((pfrag - 0.5) ** 2) >= INV_PROBA or n >= max_iterations
+    )
 
 
 def get_framentation_probability(
-	G: nx.Graph,
-	stop_condition: int | Callable[[int, float, Dict], bool],
-	fragment: Callable[[nx.Graph, Dict], None] | Callable[[nx.Graph], None],
-	stop_condition_settings: Dict | None = None,
-	fragment_settings: Dict | None = None,
-	debug: bool = False,
+    G: nx.Graph,
+    stop_condition: int | Callable[[int, float, Dict], bool],
+    fragment: Callable[[nx.Graph, Dict], None] | Callable[[nx.Graph], None],
+    stop_condition_settings: Dict | None = None,
+    fragment_settings: Dict | None = None,
+    debug: bool = False,
 ) -> Tuple[float, bool]:
-	start = time.time()
-	fragmentation_count = 0
-	pfrag = 0
-	n = 0
-	while (type(stop_condition) == int and n < stop_condition) or (
-		callable(stop_condition) 
-		and (not stop_condition(n, pfrag, stop_condition_settings))
-	):
-		if len(signature(fragment).parameters) == 2:
-			G_ = fragment(G, fragment_settings)
-		else:
-			G_ = fragment(G)
+    start = time.time()
+    fragmentation_count = 0
+    pfrag = 0
+    n = 0
+    while (type(stop_condition) == int and n < stop_condition) or (
+        callable(stop_condition)
+        and (not stop_condition(n, pfrag, stop_condition_settings))
+    ):
+        if len(signature(fragment).parameters) == 2:
+            G_ = fragment(G, fragment_settings)
+        else:
+            G_ = fragment(G)
 
-		n += 1
-		if len(G_.nodes) > 0 and not nx.is_connected(G_):
-			fragmentation_count += 1
-		pfrag = fragmentation_count / n
-	if debug:
-		print(
-			"fragmentation setttings=",
-			fragment_settings,
-			"stop_condition_settings=",
-			stop_condition_settings,
-			"with n=",
-			n,
-			"got p(frag)=",
-			pfrag,
-			1000 * (time.time() - start) / n,
-			"ms/sim",
-		)
-	return pfrag, n
+        n += 1
+        if len(G_.nodes) > 0 and not nx.is_connected(G_):
+            fragmentation_count += 1
+        pfrag = fragmentation_count / n
+    if debug:
+        print(
+            "fragmentation setttings=",
+            fragment_settings,
+            "stop_condition_settings=",
+            stop_condition_settings,
+            "with n=",
+            n,
+            "got p(frag)=",
+            pfrag,
+            1000 * (time.time() - start) / n,
+            "ms/sim",
+        )
+    return pfrag, n
+
+
+def bisection(
+    G: nx.Graph,
+    steps: int,
+    error_probability: float,
+    fragment: Callable[[nx.Graph, Dict], None] | Callable[[nx.Graph], None],
+    fragment_settings: Dict | None = None,
+    min_iterations: int = 1000,
+    max_iterations: int = 1000000,
+    debug: bool = False,
+) -> Tuple[float, int]:
+    eps = 1 - (1 - error_probability) ** (
+        1 / steps
+    )  # Compute the upper bond of error for one bisection step from the upper bond of making a mistake in the entire process
+    lower_bound = 0
+    upper_bound = 1
+    step_count = 0
+    while step_count < steps:
+        middle = (lower_bound + upper_bound) / 2
+        step_count += 1
+        pfrag, iteration_count = get_framentation_probability(
+            G,
+            bisection_stop_condition,
+            fragment,
+            stop_condition_settings={
+                "error_probability": eps,
+                "min_iterations": min_iterations,
+                "max_iterations": max_iterations,
+            },
+            fragment_settings=fragment_settings,
+            debug=debug,
+        )
+        if iteration_count == max_iterations:
+            return middle, step_count
+        elif pfrag > 0.5:
+            upper_bound = middle
+        else:
+            lower_bound = middle
+    return middle, step_count
--- tests/test_analyser.py
+++ tests/test_analyser.py
@@ -4,8 +4,13 @@ from capsidgraph.analyser.analyse import (
     bisection_stop_condition,
     get_framentation_probability,
 )
-from capsidgraph.analyser.fragment import probability_removal as probability_fragment, energy_bonds_removal, energy_nodes_removal
+from capsidgraph.analyser.fragment import (
+    probability_removal as probability_fragment,
+    energy_bonds_removal,
+    energy_nodes_removal,
+)
 from capsidgraph.analyser.util import init_nodes_energy
+
 
 class TestAnalyser(unittest.TestCase):
     def test_fragment_probability(self):
@@ -35,71 +40,75 @@ class TestAnalyser(unittest.TestCase):
         self.assertFalse(
             bisection_stop_condition(10000, 0.5, {"error_probability": 0.01})
         )
-    
+
     def test_get_fragmentation_probability(self):
         G = nx.read_adjlist("tests/testcase1.adjlist")
         pfrag, n = get_framentation_probability(
             G,
             1000,
             probability_fragment,
-            fragment_settings={"fragmentation_probability": 0.5, "fragmentation_type": "nodes"},
+            fragment_settings={
+                "fragmentation_probability": 0.5,
+                "fragmentation_type": "nodes",
+            },
         )
-        self.assertLess(pfrag,1)
-        self.assertGreater(pfrag,0)
-        self.assertEqual(n,1000)
-        
+        self.assertLess(pfrag, 1)
+        self.assertGreater(pfrag, 0)
+        self.assertEqual(n, 1000)
+
         pfrag, n = get_framentation_probability(
             G,
             bisection_stop_condition,
             probability_fragment,
-            stop_condition_settings={"error_probability": 0.01,"min_iterations": 100},
-            fragment_settings={"fragmentation_probability": 0.5, "fragmentation_type": "nodes"},
+            stop_condition_settings={"error_probability": 0.01, "min_iterations": 100},
+            fragment_settings={
+                "fragmentation_probability": 0.5,
+                "fragmentation_type": "nodes",
+            },
         )
-        self.assertLess(pfrag,1)
-        self.assertGreater(pfrag,0)
-        self.assertGreaterEqual(n,100)
-    
+        self.assertLess(pfrag, 1)
+        self.assertGreater(pfrag, 0)
+        self.assertGreaterEqual(n, 100)
+
     def test_fragment_energy_nodes(self):
         G = nx.read_adjlist("tests/testcase1.adjlist")
-        nx.set_edge_attributes(G, 1/len(G.edges), "energy")
+        nx.set_edge_attributes(G, 1 / len(G.edges), "energy")
         init_nodes_energy(G)
         for n in G.nodes:
             s = 0
-            for nei in nx.neighbors(G,n):
+            for nei in nx.neighbors(G, n):
                 s += G.edges[(n, nei)]["energy"]
             self.assertEqual(G.nodes[n]["energy"], s)
-    
+
     def test_fragment_energy_bonds(self):
         G = nx.read_adjlist("tests/testcase1.adjlist")
-        nx.set_edge_attributes(G, 1/len(G.edges), "energy")
+        nx.set_edge_attributes(G, 1 / len(G.edges), "energy")
         energy = 0.6
         G_ = energy_bonds_removal(G, {"fragmentation_energy": energy})
         remaining_energy = 0
         for e in G_.edges:
             remaining_energy += G_.edges[e]["energy"]
-        self.assertLessEqual(remaining_energy, 1-energy)
+        self.assertLessEqual(remaining_energy, 1 - energy)
 
     def test_fragment_energy_nodes(self):
         G = nx.read_adjlist("tests/testcase1.adjlist")
-        nx.set_edge_attributes(G, 1/len(G.edges), "energy")
+        nx.set_edge_attributes(G, 1 / len(G.edges), "energy")
         init_nodes_energy(G)
         fragmentation_energy = 0.5
         G_ = energy_nodes_removal(G, {"fragmentation_energy": fragmentation_energy})
         remaining_energy = 0
         for e in G_.edges:
             remaining_energy += G_.edges[e]["energy"]
-        self.assertAlmostEqual(remaining_energy, 1-fragmentation_energy,places=2)
+        self.assertAlmostEqual(remaining_energy, 1 - fragmentation_energy, places=2)
 
         for n in G.nodes:
             s = 0
-            for nei in nx.neighbors(G,n):
+            for nei in nx.neighbors(G, n):
                 s += G.edges[(n, nei)]["energy"]
             self.assertEqual(G.nodes[n]["energy"], s)
 
+    # TODO: test bisection function
 
-
-
-        
 
 if __name__ == "__main__":
     unittest.main()
